5eea2be336dae92594139d211e71b227d779d011
Andre Amor <ramorSTEM2022@gmail.com>
2025-07-03 00:27:06 -0700

    more plugins

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,38 +1,56 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        highlight = { enable = true },
-        indent = { enable = true },
-      }
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup()
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "williamboman/mason.nvim",
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = true,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").astro.setup({})
-    end,
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                highlight = { enable = true },
+                indent = { enable = true },
+            }
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup()
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        config = true,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = true,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("lspconfig").astro.setup({})
+        end,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup({})
+            vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+        end,
+    }
 }
 
