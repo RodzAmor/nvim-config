79c5cce0344353a3d1df330bd3419241ffa6fc1f
Andre Amor <ramorSTEM2022@gmail.com>
2025-07-20 23:50:14 -0700

    add latex lsp

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -6,6 +6,7 @@ return {
             require("nvim-treesitter.configs").setup {
                 highlight = { enable = true },
                 indent = { enable = true },
+                ensure_installed = { "astro", "javascript", "typescript", "html", "css" },
             }
         end,
     },
@@ -37,14 +38,32 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = true,
+        opts = {
+            ensure_installed = { "astro", "texlab" },
+        },
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            require("lspconfig").astro.setup({})
+            local lspconfig = require("lspconfig")
+            lspconfig.astro.setup({})
+            lspconfig.texlab.setup({
+                settings = {
+                    texlab = {
+                        build = {
+                            executable = "latexmk",
+                            args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                            onSave = true,
+                        },
+                        forwardSearch = {
+                            executable = "zathura",
+                            args = { "--synctex-forward", "%l:1:%f", "%p" },
+                        },
+                    },
+                },
+            })
         end,
-    },
+    }, 
     {
         "nvim-tree/nvim-tree.lua",
         dependencies = {
@@ -66,6 +85,59 @@ return {
             })
             require("telescope").load_extension("projects")
         end,
-    }
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            })
+
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.astro.setup({ capabilities = capabilities })
+            lspconfig.texlab.setup({
+                capabilities = capabilities,
+                settings = {
+                    texlab = {
+                        build = {
+                            executable = "latexmk",
+                            args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                            onSave = true,
+                        },
+                        forwardSearch = {
+                            executable = "zathura",
+                            args = { "--synctex-forward", "%l:1:%f", "%p" },
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
 
