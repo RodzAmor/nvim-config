488e4c08e0c3cacff35c058b2a8abdf0d8645f89
Andre Amor <ramorSTEM2022@gmail.com>
2025-07-21 00:55:27 -0700

    add latex snippet

---
+++ snippets/tex.lua
@@ -0,0 +1,13 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+
+return {
+  s("beg", {
+    t("\\begin{"), i(1, "env"), t({ "}", "\t" }),
+    i(2),
+    t({ "", "\\end{" }), f(function(args) return args[1][1] end, {1}), t("}")
+  }),
+}
